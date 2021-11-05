445a400e42a6bbb784d2a2b80cb4405027ca34c9
joaovitorgferreira <joao.vitor.ferreira@usp.br>
2021-11-05 17:24:07 -0300

    exercício 4 rosalid final

--- E4-rosalind.py
+++ E4-rosalind.py
@@ -4,14 +4,14 @@ elemento1 = str(lista[0])+str(lista[1])
 
 k = int(lista[3])
 n = int(elemento1)
-sequencia_pop = [0,1] #inicia a sequência desde o mês 1
+sequencia_pop = [0,1]
 indice = 1
 
 for pop in sequencia_pop:
     if len(sequencia_pop)<= n:
         sequencia_pop.append(sequencia_pop[indice-1]*k+(sequencia_pop[indice]))
         indice = indice + 1
-print(sequencia_pop)
+print(indice)
 
 
 
